7fd542502908b6b2fcd7be41250737494773911b
nofamex <nsiren517@gmail.com>
2022-06-26 20:24:26 +0700

    add environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auth } from './auth/auth.entity';
 import { AuthModule } from './auth/auth.module';
+import 'dotenv/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      entities: [Auth],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
